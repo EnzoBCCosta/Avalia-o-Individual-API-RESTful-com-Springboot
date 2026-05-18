920d2736471f9a2463027c4a131825b0cfc029ec
EnzoBCCosta <enzocarneiro2006@gmail.com>
2026-05-18 15:10:02 -0300

    feat/test pastas principais

---
+++ trabalhoindividual/src/main/java/trabalhoindividual/TrabalhoindividualApplication.java
@@ -0,0 +1,13 @@
+package trabalhoindividual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoindividualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoindividualApplication.class, args);
+	}
+
+}
---
+++ trabalhoindividual/src/test/java/trabalhoindividual/TrabalhoindividualApplicationTests.java
@@ -0,0 +1,13 @@
+package trabalhoindividual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoindividualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
