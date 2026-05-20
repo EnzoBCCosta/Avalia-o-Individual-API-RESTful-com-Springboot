6a28955429b7640ff6fa319d14d4a2d26512083a
EnzoBCCosta <enzocarneiro2006@gmail.com>
2026-05-19 21:50:07 -0300

    feat/ Classe Animal, AnimalController, AnimalService, AnimalRespository)

--- trabalhoindividual/src/main/java/trabalhoindividual/domain/Animal.java
+++ trabalhoindividual/src/main/java/trabalhoindividual/domain/Animal.java
@@ -1,5 +1,83 @@
 package trabalhoindividual.domain;
 
-public class Animal {
-    
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Animal{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String nome;
+    private String especie;   
+    private String raca;     
+    private String porte;      
+    private String sexo;
+    private String status; 
+    private Integer idade;
+
+    public Animal(String nome, String especie, Integer idade) {
+        this.nome = nome;
+        this.especie = especie;
+        this.idade = idade;
+        this.status = status;
+        this.porte = porte;
+        this.sexo = sexo;
+        this.raca = raca;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getPorte() {
+        return porte;
+    }
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
 }
